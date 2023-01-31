86b46c246ad638d24d6864d87909769d5bb50c6f
danielkuciej <biuro.symmetry@gmail.com>
2023-01-31 20:40:07 +0100

    Initial commit

---
+++ bankist_website_project4.js
@@ -0,0 +1,47 @@
+'use strict';
+
+//Elements
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const btnCloseModal = document.querySelector('.btn--close-modal');
+const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const btnLeft = document.querySelector('.slider__btn--left');
+const btnRight = document.querySelector('.slider__btn--right');
+
+const section1 = document.querySelector('#section--1');
+const section2 = document.querySelector('#section--2');
+const section3 = document.querySelector('#section--3');
+const allSection = document.querySelectorAll('.section');
+
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContent = document.querySelectorAll('.operations__content');
+const tabsContainer = document.querySelector('.operations__tab-container');
+
+const dotContainer = document.querySelector('.dots');
+const nav = document.querySelector('.nav');
+const header = document.querySelector('.header');
+const slides = document.querySelectorAll('.slide');
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+
+// Modal window
+const openModal = function (e) {
+  e.preventDefault();
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
