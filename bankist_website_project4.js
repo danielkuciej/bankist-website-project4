db9d79c024c594de2cbd85e86c0acabd14af1051
danielkuciej <biuro.symmetry@gmail.com>
2023-02-01 18:09:00 +0100

    Content field section 2 - implement guard missing click

--- bankist_website_project4.js
+++ bankist_website_project4.js
@@ -63,6 +63,9 @@ document.querySelectorAll('.nav__link').forEach(function (el) {
 tabsContainer.addEventListener('click', function (e) {
   const clicked = e.target.closest('.operations__tab');
 
+  //Guard missing click
+  if (!clicked) return;
+
   //Remove active classes
   tabs.forEach(t => t.classList.remove('operations__tab--active'));
   tabsContent.forEach(c => c.classList.remove('operations__content--active'));
