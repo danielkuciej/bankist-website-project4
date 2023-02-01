755335a930432bfcfb64bf6637deddb188b9330c
danielkuciej <biuro.symmetry@gmail.com>
2023-02-01 18:35:26 +0100

    Implement sticky navigation

--- bankist_website_project4.js
+++ bankist_website_project4.js
@@ -100,3 +100,20 @@ nav.addEventListener('mouseover', function (e) {
 nav.addEventListener('mouseout', function (e) {
   handleHover(e, 1);
 });
+
+//Sticky navigation
+const navHeight = nav.getBoundingClientRect().height;
+const stickyNav = function (entries) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) nav.classList.add('sticky');
+  else nav.classList.remove('sticky');
+};
+
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`,
+});
+
+headerObserver.observe(header);
